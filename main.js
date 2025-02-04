f6facc11e59cb37021fe285c4419f8a6311670cd
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-02-04 09:55:14 -0500

    Write tests and function for reverseString

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-module.exports = { capitalize };
+function reverseString(string) {
+    const splitString = string.split('');
+    const reverseArray = splitString.reverse();
+    return reverseArray.join('');
+}
+
+module.exports = { capitalize, reverseString };
--- main.test.js
+++ main.test.js
@@ -1,9 +1,17 @@
-const { capitalize } = require('./main');
+const { capitalize, reverseString } = require('./main');
 
 test('capitalizes "i am here"', () => {
     expect(capitalize('i am here')).toBe('I am here');
   });
 
-  test('capitalizes "the cat is snoring."', () => {
+test('capitalizes "the cat is snoring."', () => {
     expect(capitalize('the cat is snoring.')).toBe('The cat is snoring.');
-  });
+});
+
+test('reverses "to Wong Foo"', () => {
+    expect(reverseString('to Wong Foo')).toBe('ooF gnoW ot');
+});
+
+test ('reverses "Japanese anime"', () => {
+    expect(reverseString('Japanese anime')).toBe('emina esenapaJ');
+});
