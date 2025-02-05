8ee47aa2d584383c52c58ab7efdf16b799f5ba2b
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-02-05 11:49:44 -0500

    feat: add analyzeArray function and tests

--- main.js
+++ main.js
@@ -8,4 +8,55 @@ function reverseString(string) {
     return reverseArray.join('');
 }
 
-module.exports = { capitalize, reverseString };
+const calculator = {
+    add: function (x, y) {
+        return x + y;
+    },
+    subtract: function (x, y) {
+        return x - y;
+    },
+    divide: function (x, y) {
+        return x / y;
+    },
+    multiply: function (x, y) {
+        return x * y;
+    }
+}
+
+function caesarCipher(string, num) {
+    let cipher = '';
+    for (let i = 0; i < string.length; i++) {
+        const ascii = string.charCodeAt(i);
+        if (ascii > 64 && ascii < 91) {
+            cipher += String.fromCharCode(((ascii - 65 + num) % 26) + 65);
+        } else if (ascii > 96 && ascii < 123) {
+            cipher += String.fromCharCode(((ascii - 97 + num) % 26) + 97);
+        } else {
+            console.log('triggered')
+            cipher += string.charAt(i);
+        }
+    }
+    return cipher;
+};
+
+function analyzeArray(arr) {
+    let sum = 0, minimum = arr[0], maximum = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+        if (arr[i] < minimum) {
+            minimum = arr[i];
+        };
+        if (arr[i] > maximum) {
+            maximum = arr[i];
+        };
+    }
+    const obj = {
+        average: sum / arr.length,
+        min: minimum,
+        max: maximum,
+        length: arr.length
+    }
+    return obj;
+}
+
+module.exports = { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-const { capitalize, reverseString } = require('./main');
+const { capitalize, reverseString, calculator, caesarCipher, analyzeArray } = require('./main');
 
 test('capitalizes "i am here"', () => {
     expect(capitalize('i am here')).toBe('I am here');
@@ -12,6 +12,64 @@ test('reverses "to Wong Foo"', () => {
     expect(reverseString('to Wong Foo')).toBe('ooF gnoW ot');
 });
 
-test ('reverses "Japanese anime"', () => {
+test('reverses "Japanese anime"', () => {
     expect(reverseString('Japanese anime')).toBe('emina esenapaJ');
+});
+
+test('calculator adds "1, 2"', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('calculator adds "2, 3"', () => {
+    expect(calculator.add(2, 3)).toBe(5);
+});
+
+test('calculator subtracts "5, 2"', () => {
+    expect(calculator.subtract(5, 2)).toBe(3);
+});
+
+test('calculator subtracts "6, 4"', () => {
+    expect(calculator.subtract(6, 4)).toBe(2);
+});
+
+test('calculator divides "6, 2"', () => {
+    expect(calculator.divide(6, 2)).toBe(3);
+});
+
+test('calculator divides "10, 5"', () => {
+    expect(calculator.divide(10, 5)).toBe(2);
+});
+
+test('calculator multiplies "6, 2"', () => {
+    expect(calculator.multiply(6, 2)).toBe(12);
+});
+
+test('calculator multiplies "4, 8"', () => {
+    expect(calculator.multiply(4, 8)).toBe(32);
+});
+
+test('caesarCipher encrypts "Khannn!" by factor of 3', () => {
+    expect(caesarCipher('Khannn!', 3)).toBe('Nkdqqq!');
+});
+
+test('caesarCipher encrypts "xyz" by factor of 3', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+});
+
+test('object assignment', () => {
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+    });
+});
+
+test('object assignment', () => {
+    expect(analyzeArray([5,9,3,2,6])).toEqual({
+        average: 5,
+        min: 2,
+        max: 9,
+        length: 5
+    });
 });
